7cd9a19a79b9eade959af5e67d1e4719f5d0e4b5
Your Name <you@example.com>
2019-09-25 22:22:33 +0800

    Initial commit

---
+++ app/src/main/java/com/example/application/LoginActivity.java
@@ -0,0 +1,37 @@
+package com.example.application;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+    private EditText username,password;
+    private Button login,edit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        username=findViewById(R.id.username);
+        password=findViewById(R.id.password);
+        login=findViewById(R.id.login);
+        edit=findViewById(R.id.edit);
+
+        login.setOnClickListener(this);
+        edit.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.login:
+                Intent intent=new Intent(LoginActivity.this,MainActivity.class);
+                startActivity(intent);
+
+        }
+    }
+}
